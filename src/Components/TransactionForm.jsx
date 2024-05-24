ab47ab1e1ba26fb7222e1bd17370d86220e5002d
Joaquin Macbook <joaquin@gjm.com>
2024-05-24 11:37:28 -0300

    se agrega limpieza de form con modificación de props

--- src/Components/TransactionForm.jsx
+++ src/Components/TransactionForm.jsx
@@ -18,15 +18,16 @@ export const TransactionForm = () => {
       Importe: +Importe
     })
     
-    
+    setDescripcion("")
+    setImporte(0)
 
   } 
 
   return (
     <div>
       <form onSubmit={onFormSubmit}>
-        <input className='bg-zinc-700 text-white w-full rounded-lg p-3 mb-2' type="text" placeholder='ingrese una descripcion' onChange={(e)=>{setDescripcion(e.target.value)}} />
-        <input className='bg-zinc-700 text-white w-full rounded-lg p-3 mb-2' type="number" step="0.1" placeholder='ingrese un monto' onChange={(e)=>{setImporte(e.target.value)}} />
+        <input value={Descripcion} className='bg-zinc-700 text-white w-full rounded-lg p-3 mb-2' type="text" placeholder='ingrese una descripcion' onChange={(e)=>{setDescripcion(e.target.value)}} />
+        <input value={Importe} className='bg-zinc-700 text-white w-full rounded-lg p-3 mb-2' type="number" step="0.1" placeholder='ingrese un monto' onChange={(e)=>{setImporte(e.target.value)}} />
         <button className='bg-teal-400 p-3 w-full '>Enviar transacción</button>
       </form>
     </div>
